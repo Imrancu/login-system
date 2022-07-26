1bd4bdd3fe06e978b11f50e5e3140265e381402c
Imrancu <mdimran3772@gmail.com>
2022-07-26 14:58:43 +0600

    updated

--- src/components/Pages/LeftSide.js
+++ src/components/Pages/LeftSide.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import bg from '../../assets/bg.jpg'
+import logo from '../../assets/logo.png'
 
 const LeftSide = () => {
     return (
-            <div style={{backgroundImage:  `url(${bg})`}} className='fixed w-full bg-no-repeat h-screen'>
-                <div className='text-white pl-14'>
-                    <h1 class="text-3xl mt-7 font-bold">logo</h1>
-                    <p className='text-5xl font-bold my-56 ml-40'>Middle</p>
-                    <h3 class="text-2xl">© 2022 Fidia Creatives, Inc.</h3>
+            <div style={{backgroundImage:  `url(${bg})`}} className='fixed max-w-96 bg-no-repeat bg-cover min-h-screen'>
+                <div className='text-white p-10 flex flex-col justify-between'>
+                    <img className='w-14' src={logo}/>
+                    <h3 class="text-2xl object-bottom">© 2022 Fidia Creatives, Inc.</h3>
                 </div>
             </div>
     );
